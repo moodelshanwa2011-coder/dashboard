900e5002f60217c3908f568f24c826ff9960bcd2
moodelshanwa2011-coder <mood.elshanwa2011@gmail.com>
2026-04-07 05:04:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import streamlit as st
 import pandas as pd
 
-st.title("UPLOAD TEST")
+st.title("📊 Excel Dashboard")
 
-uploaded_file = st.file_uploader("Upload Excel File", type=["xlsx"])
+uploaded_file = st.file_uploader(
+    "Upload Excel File",
+    type=["xlsx"]
+)
 
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
-    st.success("Uploaded ✅")
+
+    st.success("File uploaded successfully ✅")
+
+    st.subheader("Data Preview")
     st.dataframe(df)
+
+    st.subheader("Basic Info")
+    st.write(df.describe())
